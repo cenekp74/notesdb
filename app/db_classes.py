f39de0ba41bdc273897103b2375649be7ca5d3f5
Čeněk Použník <potuznikcenek@gmail.com>
2024-01-15 22:36:39 +0100

    zaklad db

--- app/db_classes.py
+++ app/db_classes.py
@@ -10,5 +10,23 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=False, nullable=False)
+    name = db.Column(db.String(100)) # realny jmeno
+    account_created = db.Column(db.DateTime)
+    email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(60), nullable=False)
+    items = db.relationship('Item', backref='user')
     admin = db.Column(db.Integer, nullable=False, default=0)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    item_type = db.Column(db.String(10), nullable=False) # zapisky, pl, ucebnice, prezentace...
+    author = db.Column(db.String(100)) # autor - default ten kdo nahrava
+    tags = db.Column(db.String(200)) # tagy oddeleny proste mezerou
+    prof = db.Column(db.String(100)) # ucitel
+    datetime_uploaded = db.Column(db.DateTime)
+    uploaded_by = db.Column(db.Integer, db.ForeignKey('User.id'))
+    note = db.Column(db.Text) # nejaky dodatecny poznamky
+    grade = db.Column(db.String(10)) # jakoze jakej rocnik
+    subject = db.Column(db.String(10)) # predmet
+    generated_content = db.Column(db.Text) # content rozpoznany ai
