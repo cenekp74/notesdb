9120c069fd444117251a76ecd9288b0434f82308
Čeněk Použník <potuznikcenek@gmail.com>
2024-01-16 17:28:07 +0100

    registration form

--- app/forms.py
+++ app/forms.py
@@ -2,11 +2,28 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TimeField, IntegerField, ValidationError, SelectField, TextAreaField
 from wtforms_sqlalchemy.fields import QuerySelectField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms.validators import DataRequired, NumberRange, Email, Length, EqualTo
+from app.db_classes import User
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Heslo', validators=[DataRequired()])
     remember = BooleanField('Pamatuj si mě')
     submit = SubmitField('Přihlásit')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=2, max=120)])
+    password = PasswordField('Heslo', validators=[DataRequired()])
+    confirm_password = PasswordField('Potvrdit heslo', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Registrovat')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Uživatelské jméno je již zabrané')
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('Email je již používán')
