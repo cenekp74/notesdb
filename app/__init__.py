bf3589a22c7975882bc9112dc3269fadd439cee6
Čeněk Použník <potuznikcenek@gmail.com>
2024-01-26 22:14:51 +0100

    added subject to search result + subjects and types made lowercase

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-VALID_ITEM_TYPES = ['Zápisky', 'Učebnice', 'Prezentace', 'Jiné']
-VALID_SUBJECTS = ['', 'Český jazyk a literatura', 'Matematika', 'Biologie', 'Geografie (zeměpis)', 'Dějepis', 'Anglický jazyk', 'Francouzský jazyk', 'Německý jazyk', 'Fyzika', 'Chemie', 'Společenské vědy', 'Informatika']
+VALID_ITEM_TYPES = ['zápisky', 'učebnice', 'prezentace', 'jiné']
+VALID_SUBJECTS = ['', 'český jazyk a literatura', 'matematika', 'biologie', 'geografie (zeměpis)', 'dějepis', 'anglický jazyk', 'francouzský jazyk', 'německý jazyk', 'fyzika', 'chemie', 'společenské vědy', 'informatika']
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5e72ba27fc6a863eed13c27e6750bd25ab0be9ff55ac0e34823d966c1ce4896026992f2639857117'
