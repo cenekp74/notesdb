c544094190c9d4c2c18e3a62e5436104ccd76b3c
Čeněk Použník <potuznikcenek@gmail.com>
2024-02-01 19:52:58 +0100

    async mail sending

--- app/mail.py
+++ app/mail.py
@@ -3,9 +3,9 @@ from flask_mail import Message
 from app import app, mail
 from flask import render_template
 
-def send_email(to, subject, template, **kwargs):
+async def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['MAIL_SUBJECT_PREFIX'] + subject,
     sender=app.config['MAIL_SENDER'], recipients=[to])
     msg.body = render_template('email/' + template + '.txt', **kwargs)
     msg.html = render_template('email/' + template + '.html', **kwargs)
-    mail.send(msg)
+    await mail.send(msg)
