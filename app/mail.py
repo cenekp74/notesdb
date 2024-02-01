b08c673056bce0094f91200b077718162cfd803d
Čeněk Použník <potuznikcenek@gmail.com>
2024-02-01 19:50:09 +0100

    removed threading from mail

--- app/mail.py
+++ app/mail.py
@@ -3,15 +3,9 @@ from flask_mail import Message
 from app import app, mail
 from flask import render_template
 
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
-
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['MAIL_SUBJECT_PREFIX'] + subject,
     sender=app.config['MAIL_SENDER'], recipients=[to])
     msg.body = render_template('email/' + template + '.txt', **kwargs)
     msg.html = render_template('email/' + template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[app, msg])
-    thr.start()
-    return thr
+    mail.send(msg)
