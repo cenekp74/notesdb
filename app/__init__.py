bd62a87359123458cd8e0ccee88e622fe6a86ab0
Čeněk Použník <potuznikcenek@gmail.com>
2024-02-04 12:19:22 +0100

    mail

--- app/__init__.py
+++ app/__init__.py
@@ -17,12 +17,12 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SECURITY_PASSWORD_SALT'] = os.getenv('SECURITY_PASSWORD_SALT')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_SERVER'] = 'smtp.forpsi.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_SENDER'] = 'notesdb.cz@gmail.com'
+app.config['MAIL_SENDER'] = 'mail@notesdb.cz'
 app.config['MAIL_SUBJECT_PREFIX'] = 'NotesDB '
 
 from flask_mail import Mail
